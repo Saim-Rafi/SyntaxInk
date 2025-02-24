115736d94de14a8353088c24af84f76946647029
Saim Rafi <143498300+Saim-Rafi@users.noreply.github.com>
2025-02-24 23:58:00 +0530

    added onclick on logo

--- frontend/src/components/EditorNavbar.jsx
+++ frontend/src/components/EditorNavbar.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import logo from "../images/logo.png"
+import React from "react";
+import logo from "../images/logo.png";
 import { FiDownload } from "react-icons/fi";
-
+import { useNavigate } from "react-router-dom";
 
 const EditorNavbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
   return (
     <>
       <div className="EditorNavbar flex items-center justify-between px-[100px] h-[80px] bg-[#141414]">
         <div className="logo">
-          <img className='w-[150px] cursor-pointer' src={logo} alt="" />
+          <img
+            className="w-[150px] cursor-pointer"
+            src={logo}
+            alt=""
+            onClick={handleLogoClick}
+          />
         </div>
-        <p>File / <span className='text-[gray]'>My first project</span></p>
-        <i className='p-[8px] btn bg-black rounded-[5px] cursor-pointer text-[20px]'><FiDownload /></i>
+        <p>
+          File / <span className="text-[gray]">My first project</span>
+        </p>
+        <i className="p-[8px] btn bg-black rounded-[5px] cursor-pointer text-[20px]">
+          <FiDownload />
+        </i>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default EditorNavbar
- 
+export default EditorNavbar;
